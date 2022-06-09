e6eb1d25593c706f3f52de3b3a4ee0242a28a6b9
Augustin <adebureaux@gmail.com>
2022-06-09 10:41:35 +0200

    save

--- main.cpp
+++ main.cpp
@@ -42,22 +42,30 @@ int main(void)
 		rev_it = 3 + myvector.rbegin();
 
 		std::cout << "The fourth element from the end is: " << *rev_it << '\n';
-
-		return 0;
 	}
+	// {
+	// 	ft::vector<int> myvector;
+	// 	for (int i=0; i<10; i++) myvector.push_back(i);	// myvector: 0 1 2 3 4 5 6 7 8 9
+
+	// 	typedef ft::vector<int>::iterator iter_type;
+
+	// 	ft::reverse_iterator<iter_type> rev_it;
+
+	// 	rev_it = 3 + myvector.rbegin();
+
+	// 	std::cout << "The fourth element from the end is: " << *rev_it << '\n';
+	// }
 	{
 		ft::vector<int> myvector;
-		for (int i=0; i<10; i++) myvector.push_back(i);	// myvector: 0 1 2 3 4 5 6 7 8 9
+		for (int i=0; i<10; i++) 
+			myvector.push_back(i);  // myvector: 0 1 2 3 4 5 6 7 8 9
 
 		typedef ft::vector<int>::iterator iter_type;
 
-		ft::reverse_iterator<iter_type> rev_it;
+		ft::reverse_iterator<iter_type> rev_iterator = myvector.rbegin();
 
-		rev_it = 3 + myvector.rbegin();
-
-		std::cout << "The fourth element from the end is: " << *rev_it << '\n';
-
-		return 0;
+		std::cout << "The fourth element from the end is: " << rev_iterator[3] << '\n';
 	}
+	
 	return (0);	
 }
